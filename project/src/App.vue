8c7bd3b140169b807a87d72e30ca81f7e9059978
Abel Henry-Lapassat <98323161+Jvaljer@users.noreply.github.com>
2024-10-20 19:53:19 +0200

    Vue init + Project main orga

---
+++ project/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Login />
+  <div class="window-container" hidden> <!-- This must only be displayed if the "Login" component is on state #connected# -->
+    <Navbar />
+    <Content />
+  </div>
+</template>
+
+
+<!-- LOCAL SCRIPT -->
+<script setup>
+
+import Login from './components/Login.vue';
+import Navbar from './components/Navbar.vue';
+import Content from './components/Content.vue';
+
+import { useToast } from 'vue-toastification';
+import { red, computed, onMounted } from 'vue'; //check which one are really needed here
+
+</script>
+
+<!-- LOCAL STYLES -->
+<style>
+
+</style>
---
+++ project/src/components/Content.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- This is the CORE component of the app -> It contains the content, and is the one handling the changing of those -->
+    <!-- 
+        For the following components, only one is displayed at a time !
+        On each reload there must be the selection of which must be displayed, and which shall be hidden. 
+    -->
+
+    <Dashboard />
+    <Project /> <!-- Needs a precision on WHICH project is opened -->
+</template>
+  
+  
+<!-- LOCAL SCRIPT -->
+<script setup>
+
+import Dashboard from './Dashboard.vue';
+import Project from './Project.vue';
+
+</script>
+  
+<!-- LOCAL STYLES -->
+<style>
+</style>
